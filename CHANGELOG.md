ead09dc768c4d9c3adc9c216cfb1e2d967e26183
Alfredo Barros <coomcs@gmail.com>
2025-10-07 07:48:17 -0300

    feat: namespace utility scripts under TheyCallMeAlfredUnity
    - Wrap AnimationTrigger, CombineMesh, SimpleRotation, and TextureSwapper in namespace
    - Update CHANGELOG.md to document the changes

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+## 1.0.0 - 07-10-2025
+
+### First release
+
+### Added
+- Added `TheyCallMeAlfredUnity` namespace to all utility scripts:
+  - `AnimationTrigger.cs`
+  - `CombineMesh.cs`
+  - `SimpleRotation.cs`
+  - `TextureSwapper.cs`
---
+++ TheyCallMeAlfred/Scripts/Utils/SimpleRotation.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace TheyCallMeAlfredUnity
+{
+    public class SimpleRotation : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 30f;
+    [SerializeField] private Vector3 rotationAxis = Vector3.up;
+
+    void Update()
+    {
+        transform.Rotate(rotationAxis * rotationSpeed * Time.deltaTime);
+    }
+}
+}
